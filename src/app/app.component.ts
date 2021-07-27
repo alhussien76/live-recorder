37b4b45128386f4f0ab1e1a0a6a5686d1165413a
MustafaGaber <mus.gaber@gmail.com>
2021-07-27 03:28:50 +0200

    Screen recorder class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,105 +1,30 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import * as RecordRTC from 'recordrtc';
-declare var navigator: any;
+import { ScreenRecorder } from 'src/util/ScreenRecorder';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   @ViewChild('videoRef', { static: false }) video: ElementRef;
   @ViewChild('startRecordingRef', { static: false }) startButton: ElementRef;
   @ViewChild('stopRecordingRef', { static: false }) stopButton: ElementRef;
-  recorder: any;
+
+  screenRecorder = new ScreenRecorder();
+  
   constructor() { }
-  ngOnInit(): void {
-
-  }
+ 
   startRecording(): void {
-    console.log(this.video);
-    console.log(this.video.nativeElement);
-    this.startButton.nativeElement.disabled = true;
-    this.captureScreen((screen) => {
-      this.video.nativeElement.srcObject = screen;
-      this.recorder = new RecordRTC(screen, {
-        type: 'video'
-      });
-
-      this.recorder.startRecording();
-      // release screen on stopRecording
-      this.recorder.screen = screen;
-
-      this.stopButton.nativeElement.disabled = false;
+    this.screenRecorder.startRecording();
+    this.screenRecorder.bolb$.subscribe(blob => {
+        this.video.nativeElement.src = URL.createObjectURL(blob);
     });
   }
+
   stopRecording(): void {
-    this.stopButton.nativeElement.disabled = true;
-    this.recorder.stopRecording(this.stopRecordingCallback);
+    this.screenRecorder.stopRecording();
   }
-  invokeGetDisplayMedia(success, error): void {
-    const displaymediastreamconstraints = {
-      video: {
-        displaySurface: 'monitor', // monitor, window, application, browser
-        logicalSurface: true,
-        cursor: 'always' // never, always, motion
-      }
-    };
-    // above constraints are NOT supported YET
-    // that's why overriding them
-    // const displaymediastreamconstraints = {
-    //   video: true
-    // };
-
-    if (navigator.mediaDevices.getDisplayMedia) {
-      navigator.mediaDevices.getDisplayMedia(displaymediastreamconstraints).then(success).catch(error);
-    }
-    else {
-      navigator.getDisplayMedia(displaymediastreamconstraints).then(success).catch(error);
-    }
-  }
-
-  captureScreen(callback): void {
-    this.invokeGetDisplayMedia((screen) => {
-      this.addStreamStopListener(screen, () => {
-        this.stopButton.nativeElement.click();
-      });
-      callback(screen);
-    }, (error) => {
-      console.error(error);
-      alert('Unable to capture your screen. Please check console logs.\n' + error);
-    });
-  }
-
-  stopRecordingCallback = () => {
-    console.log(this.video);
-    this.video.nativeElement.src = null;
-    this.video.nativeElement.srcObject = null;
-    this.video.nativeElement.src = URL.createObjectURL(this.recorder.getBlob());
-    this.recorder.screen.stop();
-    this.recorder.destroy();
-    this.recorder = null;
-    this.startButton.nativeElement.disabled = false;
-  }
-
-  addStreamStopListener(stream, callback): void {
-    stream.addEventListener('ended', () => {
-      callback();
-      callback = () => { };
-    }, false);
-    stream.addEventListener('inactive', () => {
-      callback();
-      callback = () => { };
-    }, false);
-    stream.getTracks().forEach((track) => {
-      track.addEventListener('ended', () => {
-        callback();
-        callback = () => { };
-      }, false);
-      track.addEventListener('inactive', () => {
-        callback();
-        callback = () => { };
-      }, false);
-    });
-  }
+  
 }
+
